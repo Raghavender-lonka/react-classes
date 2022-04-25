36ca0fa60bb49a6474e332af7f77d04336cb4d4a
Raghavender Reddy Lonka <96302103+Raghavender-lonka@users.noreply.github.com>
2022-04-25 21:12:54 +0530

    Add files via upload

---
+++ src/HigherOrder/App.js
@@ -0,0 +1,16 @@
+// import React, { useCallback, useEffect, useState } from "react"
+// import MemoReact from "./components/ReactMemo/ReactMemo"
+import React from "react"
+import HigherOrder from "./components/HigherOrder/HigherOrder"
+import KeyDown from "./components/HigherOrder/KeyDown"
+
+function App() {
+  return (
+    <div>
+      <HigherOrder />
+      <KeyDown />
+    </div>
+  )
+}
+
+export default App
