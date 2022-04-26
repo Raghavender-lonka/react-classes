9cf7cb97cb4688b14f9f6ed80f57ab7fe9fc4d1c
Raghavender Reddy Lonka <96302103+Raghavender-lonka@users.noreply.github.com>
2022-04-26 21:12:34 +0530

    26-04-22 axios/fetch

---
+++ src/axios/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import AxiosComp from "./components/axios/axiosComp"
+
+function App() {
+  return (
+    <div>
+      <AxiosComp />
+    </div>
+  )
+}
+
+export default App
